500ea48b29bc542f33d055f2cb8bc0a6d6f521a8
Sameeha <rahman.sameeha@gmail.com>
2020-05-31 23:46:27 +0530

    Updates

---
+++ precache-manifest.7834ab80b1b05302184a0a8041ebb0fd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa41851bb8afee165e969cefc29845bb",
+    "url": "/eat-smeeshy/index.html"
+  },
+  {
+    "revision": "f78ae4622896b6414646",
+    "url": "/eat-smeeshy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4f8a64afbfb59af7cf6a",
+    "url": "/eat-smeeshy/static/css/main.819720b9.chunk.css"
+  },
+  {
+    "revision": "f78ae4622896b6414646",
+    "url": "/eat-smeeshy/static/js/2.a94c7601.chunk.js"
+  },
+  {
+    "revision": "bfa34c994aa4cec43b728b418f1ce60c",
+    "url": "/eat-smeeshy/static/js/2.a94c7601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8a64afbfb59af7cf6a",
+    "url": "/eat-smeeshy/static/js/main.22192985.chunk.js"
+  },
+  {
+    "revision": "b74d784063d20d356656",
+    "url": "/eat-smeeshy/static/js/runtime-main.00d8c7fe.js"
+  },
+  {
+    "revision": "50f756e8c6dd20695770d91d01d01e1e",
+    "url": "/eat-smeeshy/static/media/Chocolate_Drink.50f756e8.ttf"
+  },
+  {
+    "revision": "ba267f89d82ed58d9fbac4b62aa3015a",
+    "url": "/eat-smeeshy/static/media/logo.ba267f89.svg"
+  }
+]);
